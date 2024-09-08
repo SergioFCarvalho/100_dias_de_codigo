4de698680d209a58fc0f6f266fcbf597da9470dc
Sérgio Freire <sergiocarvalhotec@gmail.com>
2024-09-08 17:47:07 -0300

    Dia 4 e 5

--- Dia 4 e 5/dia4_e_5.py
+++ Dia 4 e 5/dia4_e_5.py
@@ -1,14 +1,41 @@
-n1 = float(input('Insira o primeiro número: '))
-op = input('Insira a operação: +, -, /, *: ')
-n2 = float(input('Insira o segundo número: '))
+def adicao(x, y):
+    return x + y
 
-if op == '+':
-    result = n1 + n2
-if op == '-':
-    result = n1 - n2
-if op == '/':
-    result = n1/n2
-if op == '*':
-    result = n1*n2
 
-print('O resultado é ', result)
+def subtracao(x, y):
+    return x - y
+
+
+def multiplicacao(x, y):
+    return x * y
+
+
+def divisao(x, y):
+    return x / y
+
+
+def calculadora():
+    print('Selecione a operação')
+    print('1 - Adição')
+    print('2 - Subtração')
+    print('3 - Multiplicação')
+    print('4 - Divisão')
+
+    print("---" * 10)
+    opcao = input("Digite o número da operação: ")
+
+    if opcao == '1':
+        print(f'{num1} + {num2} = {adicao(num1, num2)}')
+    if opcao == '2':
+        print(f'{num1} - {num2} = {subtracao(num1, num2)}')
+    if opcao == '3':
+        print(f'{num1} * {num2} = {multiplicacao(num1, num2)}')
+    if opcao == '4':
+        print(f'{num1} / {num2} = {divisao(num1, num2)}')
+
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+print("---" * 10)
+
+calculadora()
